369e2f526055d1f9d5f7f81f29c81a968c626a7f
p0keT <p0keT@ukr.net>
2019-11-25 22:38:49 +0200

    Initial commit

---
+++ src/main/java/com/epam/pdp/recognitionobject/RecognitionObjectApplication.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitionobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognitionObjectApplication.class, args);
+    }
+
+}
